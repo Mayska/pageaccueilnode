80af97abc312594fae071817ed16b99691d688f2
Mayska <alexandre.meunier31@gmail.com>
2019-09-23 18:05:57 +0200

    Ajout du dossier static public et modification de l'url '/'

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile('./public/index.html')
 })
 
 app.listen(3000, function () {
